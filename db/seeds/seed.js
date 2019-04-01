fd2c78d1a06a9c6ec887d94ce53b876eaae99e3a
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-04-01 16:29:15 +0100

    Add seeding for articles and comments

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,41 @@
-const { topicsData, usersData } = require('../data');
+const {
+  topicsData, usersData, articlesData, commentsData,
+} = require('../data');
+const {
+  jsToPsqlTimestamp,
+  renameKeysOfObjects,
+  getKeyToKeyPairing,
+  replaceKeysOfObject,
+} = require('../../utils/data-normalisation');
 
 exports.seed = (knex, Promise) => knex.migrate
   .rollback()
   .then(() => knex.migrate.latest())
   .then(() => knex.insert(topicsData).into('topics'))
-  .then(() => knex.insert(usersData).into('users'));
+  .then(() => knex.insert(usersData).into('users'))
+  .then(() => {
+    const articleDataISOTimestamp = jsToPsqlTimestamp(articlesData);
+    return knex
+      .insert(articleDataISOTimestamp)
+      .into('articles')
+      .returning('*');
+  })
+  .then((articles) => {
+    // Convert from "belongs_to" (article title) to "article_id"
+    const articleTitleIDPairs = getKeyToKeyPairing(articles, 'title', 'article_id');
+    const commentsDataArticleID = replaceKeysOfObject(
+      commentsData,
+      'belongs_to',
+      'article_id',
+      articleTitleIDPairs,
+    );
+      // Convert timestamp to ISO (PostgreSQL accepted)
+    const commentsDataISOTimestamp = jsToPsqlTimestamp(commentsDataArticleID);
+    // Rename "created_by" field to "author"
+    const renamedBelongToComments = renameKeysOfObjects(
+      commentsDataISOTimestamp,
+      'created_by',
+      'author',
+    );
+    return knex.insert(renamedBelongToComments).into('comments');
+  });
